7aea633337a5dc642264b634924c0d5ad1de6288
Anton Sheremet <anton.sheremet@itechart-group.com>
2018-12-06 21:09:43 +0300

    corrected ProgressIndicator

--- lib/view/main.dart
+++ lib/view/main.dart
@@ -12,16 +12,21 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primaryColor: Colors.white,
         ),
-        home: FutureBuilder<StationList>(
-          future: model.fetchStations(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return StationListWidget(stations: snapshot.data.stations);
-            } else if (snapshot.hasError) {
-              return Text("${snapshot.error}");
-            }
-            return CircularProgressIndicator();
-          },
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('List Mode'),
+          ),
+          body: FutureBuilder<StationList>(
+            future: model.fetchStations(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return StationListWidget(stations: snapshot.data.stations);
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+              return Center(child: CircularProgressIndicator());
+            },
+          ),
         ),
       );
 }
@@ -32,43 +37,34 @@ class StationListWidget extends StatelessWidget {
   StationListWidget({@required this.stations}) : super();
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('List Mode'),
-      ),
-      body: ListView.builder(
+  Widget build(BuildContext context) => ListView.builder(
         itemCount: stations.length,
-        itemBuilder: (context, index) {
-          return Column(
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.all(8),
-                child: Text(
-                  stations[index].name,
-                  style: TextStyles.BIG,
+        itemBuilder: (context, index) => Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Text(
+                    stations[index].name,
+                    style: TextStyles.BIG,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(12, 4, 0, 4),
-                child: Text(
-                  stations[index].latitude.toString(),
-                  style: TextStyles.NORMAL_ITALIC,
+                Padding(
+                  padding: EdgeInsets.fromLTRB(12, 4, 0, 4),
+                  child: Text(
+                    stations[index].latitude.toString(),
+                    style: TextStyles.NORMAL_ITALIC,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(12, 4, 0, 8),
-                child: Text(
-                  stations[index].longitude.toString(),
-                  style: TextStyles.NORMAL_ITALIC,
+                Padding(
+                  padding: EdgeInsets.fromLTRB(12, 4, 0, 8),
+                  child: Text(
+                    stations[index].longitude.toString(),
+                    style: TextStyles.NORMAL_ITALIC,
+                  ),
                 ),
-              ),
-            ],
-          );
-        },
-      ),
-    );
-  }
+              ],
+            ),
+      );
 }
